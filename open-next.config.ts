ce8d9bcabb2251d08df68cf19a0c9a95154cc1ad
nodeshells <kenta.job09@gmail.com>
2025-07-12 14:23:00 +0900

    Update CloudFlare Workersに関する修正

---
+++ open-next.config.ts
@@ -0,0 +1,6 @@
+import { defineCloudflareConfig } from "@opennextjs/cloudflare";
+
+export default defineCloudflareConfig({
+  // Additional configuration can be added here
+  // See: https://opennext.js.org/cloudflare/configuration
+});
